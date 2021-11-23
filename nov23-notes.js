5ae5760a57396e584d9f5302cc94b82867f8de86
Matthew Lewis <matthew.d.lewis64@gmail.com>
2021-11-23 10:51:26 -0600

    added examples

--- nov23-notes.js
+++ nov23-notes.js
@@ -1,12 +1,12 @@
 // Erstwhile, the legislature determined that being at the bar is not necessary in order to be served, but all the remaining conditions are still valid
 // Change the expression assigned to cashOnHand to make isAtBar optional  while still requiring the remaining values to be true
 
-let isAtBar = true;
+let isAtBar = false;
 let bac = .00;
 let age = 22;
-let cashOnHand = 4.99;
+let cashOnHand = 7.99;
 
-
+/* Original code
 let canHaveADrink = (isAtBar && bac < .08 && age >= 21 && cashOnHand >= 5.00) // true && false && true && true --> false
 
 if (canHaveADrink){ // true or false
@@ -14,3 +14,16 @@ if (canHaveADrink){ // true or false
 }else{ // it was not true
     console.log("Sorry buddy, no brew for u");
 }
+*/
+
+// We could delete isAtBar as that condition doesn't matter anymore, but it may be useful data to keep
+let canHaveADrink = (bac < .08 && age >= 21 && cashOnHand >= 5.00) // true && false && true && true --> false
+
+if (canHaveADrink && isAtBar) { // true or false
+    console.log("Have a brewski my dude!");
+} else if (canHaveADrink && !isAtBar) { // it was not true
+    console.log("Have a brewski my dude!");
+} else {
+    console.log("Sorry buddy, no brew for u");
+}
+
--- workingWithTypes.js
+++ workingWithTypes.js
@@ -24,13 +24,14 @@ let firstLetterCodeup = sample.indexOf('C');
 let subSample = sample.substring(firstLetterCodeup, 12);
 console.log(subSample);
 
-// Ex 3
+////////////////////////////////////// Ex 3 //////////////////////////////////////////
 
 /* You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it),
 and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay? */
 const pricePerDay = 3;
 let total = pricePerDay * (3 + 5 + 1)
 console.log(total);
+
 
 /* Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour.
 Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours
@@ -41,10 +42,12 @@ const facebook = 350;
 let payment = (facebook * 10) + (google * 6) + (amazon * 4);
 console.log(payment);
 
+
 // A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
 const classSize = 30;
 let openSeats = 20;
 let conflict = true;
+
 function enroll() {
     if (openSeats < classSize && conflict === false) {
         console.log("you can enroll")
@@ -52,31 +55,37 @@ function enroll() {
         console.log("you cannot enroll");
     }
 }
+
 enroll();
+
+
 /* A product offer can be applied only if a person buys more than 2 items, and the offer has not expired.
 Premium members do not need to buy a specific amount of products. */
 
 // Constants for checking expiration
 const expiration = new Date(2022, 9, 22);
 const todaysDate = new Date();
-// User or cashier/basket would input these items
-let itemsBought = 5;
-let premium = false;
+// User/cashier/shopify/etc. would input these items
+let itemsBought = 1;
+// Is the user a Premium member? If true, then itemsBought doesn't matter
+let premium = true;
 
-function offer () {
-    if ((itemsBought >= 2 || premium === true) && todaysDate < expiration) {
+function offer() {
+    if ((itemsBought > 2 || premium === true) && todaysDate < expiration) {
         console.log("Offer applied");
     } else {
         console.log("Offer not applied");
     }
 }
+
 offer();
 
 // Ex 4
-/*
-var username = 'codeup';
-var password = 'notastrongpassword';
 
+const username = 'codeup';
+const password = 'notastrongpasswordadfafas';
+
+/*
 Create a variable that holds a boolean value for each of the following conditions:
 the password must be at least 5 characters
 the password must not include the username
@@ -84,7 +93,34 @@ the username must be no more than 20 characters
 neither the username or password can start or end with whitespace
 */
 
-let checkMinCharLength = true;
-let checkNotUserName = true;
-let checkMaxCharLength = true;
-let checkWhitespace = true;
+function checkCredentials(username, password) {
+    // Check length of password
+    if (password.length >= 5) {
+        console.log("password length good");
+    } else {
+        return console.log("password too short");
+    }
+
+    // Check that password is not the same as username
+    if (password !== username) {
+        console.log("password does not equal username");
+    } else {
+        return console.log("password cannot be the same as username");
+    }
+
+    // Check that username is not longer than 20 characters
+    if (username.length <= 20) {
+        console.log("username length good");
+    } else {
+        return console.log("username too long");
+    }
+
+    // Check for whitespace
+    if (username.indexOf(' ') === 0 && password.indexOf(' ') === 0) {
+        console.log("whitespace check good");
+    } else {
+        return console.log("whitespace not allowed");
+    }
+}
+
+checkCredentials(username, password);
