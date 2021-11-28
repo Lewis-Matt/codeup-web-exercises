6a937802947e931e86de6592bc7383fd9d8739ab
Matthew Lewis <matthew.d.lewis64@gmail.com>
2021-11-28 17:59:15 -0600

    Added hositing-example.js

---
+++ js/hoisting-example.js
@@ -0,0 +1,17 @@
+'use strict';
+
+sayHello1(); // Greetings from sayHello1.
+sayHello2(); // Uncaught TypeError: Property 'sayHello2' of object [object Object] is not a function
+
+// declare a function in the global scope
+function sayHello1 () {
+    alert('Greetings from sayHello1.');
+}
+
+// assign a function to a variable named sayHello2
+var sayHello2 = function () {
+    alert('Greetings from sayHello2.');
+};
+// As you can see, even though the sayHello1 function is called before the declaration, the declaration is hoisted to the
+// top and is therefore accessible. However, in the case of sayHello2, where a function is assigned to a variable, the
+// function will not be available until after the declaration.
