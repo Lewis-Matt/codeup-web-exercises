ef1ed809e6c1c831644afcdb9f0a514afe2fcf03
Matthew Lewis <matthew.d.lewis64@gmail.com>
2021-12-02 11:08:55 -0600

    Note taking, finished JS w/ HTML exercises

---
+++ js/12-2-Notes.js
@@ -0,0 +1,6 @@
+let maxSpots = 20;
+let availableSpots = 0;
+while(availableSpots < maxSpots) {
+    availableSpots++
+    console.log(`There are ${availableSpots} spot(s) open out of a max of ${maxSpots} spots.`)
+}
--- js/external.js
+++ js/external.js
@@ -3,13 +3,16 @@ console.log("Hello from external JavaScript");
 
 
 // Use the alert function to show a message that says 'Welcome to my Website!'.
-
+alert('Welcome to my website');
 // Use a prompt to ask for the user's favorite color. Use the user's response to alert a message that says that the color
 // entered is your favorite color too.
-
 // For example, if the user enters "blue", your code should alert a message that says:
 // "Great, blue is my favorite color too!"
+let favoriteColor = prompt('What is your favorite color?');
+alert(`${favoriteColor} is my favorite color too!`);
+
 
 // Complete exercise 3 from the previous lesson, but write your code in the external.js file instead of in the console.
 // When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
 // Use an alert to show the results of each problem.
+Which exercise is this refering to? The previous lesson, ex 3, never asks for an number input
