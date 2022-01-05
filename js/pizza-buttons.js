c1cf30be00f3c8c0ce2e20e1ca96b3cc5eb6e587
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-01-05 10:00:21 -0600

    btnNext works for the first tab, still need to fix btnPrevious and btnNext on 2nd tab

--- js/pizza-buttons.js
+++ js/pizza-buttons.js
@@ -6,7 +6,8 @@ Basic syntax is: $(selector).action()
     A (selector) to "query (or find)" HTML elements
     A jQuery action() to be performed on the element(s)
 */
-
+// I had a lot of time troubleshooting, the error was that the 'active' class was associated with the anchor tag. By
+// moving the 'active' (for the first element - Pizza Crust) to the li (that contains the anchor tag), everything worked.
 $('.btnNext').click(function () {
     $('.nav-tabs > .active').next('li').find('a').trigger('click');
 });
@@ -14,3 +15,8 @@ $('.btnNext').click(function () {
 $('.btnPrevious').click(function () {
     $('.nav-tabs > .active').prev('li').find('a').trigger('click');
 });
+
+
+
+
+
