11d3d9a804e0f0a2769b7607212209ccdad91ba5
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-01-12 09:40:45 -0600

    Fixed typos and grammar in comments

--- js/Notes/hoisting-example.js
+++ js/Notes/hoisting-example.js
@@ -15,5 +15,5 @@ var sayHello2 = function () {
 // As you can see, even though the sayHello1 function is called before the declaration, the declaration is hoisted to the
 // top and is therefore accessible. However, in the case of sayHello2, where a function is assigned to a variable, the
 // function will not be available until after the declaration.
-// Due to the way hoisting works, it is best practice to put variable declarations at the beginning of a function and
-// not spread them throughout so it is easier to see how things are working.
+// Due to the way hoisting works, it is best practice putting variable declarations at the beginning of a function and
+// not spread them throughout, so it is easier to see how things are working.
