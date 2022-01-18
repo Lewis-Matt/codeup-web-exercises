a70ded72ee294c9ce9d7fc5ee4859025bc17b67b
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-01-18 11:47:15 -0600

    more jQuery notes, jQuery exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,6 +1,19 @@
 "use strict";
 $(document).ready(function() {
-
+    // Add an alert that says the pages has finished loading
     alert("The DOM has finished loading.")
+    // ID SELECTORS
+    // Use jQuery to select an element by the id. Alert the contents of the element.
+
+    // CLASS SELECTORS
+    // Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
+
+    //ELEMENT SELECTORS
+    // Using jQuery, set the font-size of all li elements to 20px.
+
+    // Craft selectors that highlight all the h1, p, and li elements.
+
+    // MULTIPLE SELECTORS
+    // Combine your selectors that highlight all the h1, p, and li elements.
 
 });
