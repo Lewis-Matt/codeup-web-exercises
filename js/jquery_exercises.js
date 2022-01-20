b72a6d0d1632857b74f746e6f5991b78b69defb7
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-01-20 09:55:52 -0600

    1-20 exercise, using regex

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -13,8 +13,8 @@ $(document).ready(function() {
     // Using jQuery, set the font-size of all li elements to 20px.
     $('li').css('font-size', '20px');
     // Craft selectors that highlight all the h1, p, and li elements.
-    $('h1, p, li').css('background-color', '#fffd7a');
     // MULTIPLE SELECTORS
     // Combine your selectors that highlight all the h1, p, and li elements.
+    $('h1, p, li').css('background-color', '#fffd7a');
 
 });
