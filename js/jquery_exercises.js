5cfcad6a3fd3ccb3866c0f313939fc0fce516a23
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-01-20 10:58:01 -0600

    added notes, finished exercise 3

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -14,16 +14,28 @@ $(document).ready(function() {
     $('.codeup').css('border', 'solid 1px red');
     //ELEMENT SELECTORS
     // Using jQuery, set the font-size of all li elements to 20px.
-    $('li').css('font-size', '20px');
+    // $('li').css('font-size', '20px');
     // Craft selectors that highlight all the h1, p, and li elements.
     // MULTIPLE SELECTORS
     // Combine your selectors that highlight all the h1, p, and li elements.
     $('h1, p, li').css('background-color', '#fffd7a');
 
     // EXERCISE 3
-
-
-
-
-
+    // Add jQuery code that will change the background color of a h1 element when clicked.
+    $('#clickMe').click(function(){
+        $(this).css('background-color','#de5656');
+    })
+    // Make all paragraphs have a font size of 18px when they are double-clicked.
+    $('p').dblclick(function(){
+        $(this).css('font-size', '18px');
+    })
+    // Set all li text color to red when the mouse is hovering; reset to black when it is not.
+    $('li').hover(
+        function() {
+            $(this).css('color', '#F00');
+        },
+        function() {
+            $(this).css('color', '#000');
+        }
+    )
 });
