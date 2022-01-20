dc244b9448e3a3f0e46442d08430436bd178777d
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-01-20 09:55:52 -0600

    1-20 exercise, using regex

--- js/Notes/warmup-exercises.js
+++ js/Notes/warmup-exercises.js
@@ -3,7 +3,7 @@
 let namesArray = ["Ryan", "Kieran", "Jason", "Mary", "Jo", "Jake"];
 
 // ES6 version
-let filtered = namesArray.filter(string =>string.length === 4);
+let filtered = namesArray.filter(string => string.length === 4);
 console.log(filtered);
 
 // For Loop Version
@@ -18,3 +18,23 @@ function namesWithFourLetters(arr) {
 }
 
 console.log(namesWithFourLetters(namesArray));
+
+// 20 JANUARY
+console.log('----------20 JANUARY----------')
+// write a function that accepts a string and checks whether each string is upperCase or lowerCase and returns "letter  [insert letter] is uppercase/lowercase", accordingly.  Bonus: return "is not a character" if it's not a letter
+
+// I was having all sorts of confusion with the regex test() method. It returns true or false; I was trying to use if(regex.test(element) === element), which is stupid...
+let caseChecker = function (str) {
+    let strArr = str.split('');
+    const regex = /[a-zA-Z]/;
+    strArr.forEach((element) => {
+        if (regex.test(element) === false) {
+            console.log(`${element} is not a letter`)
+        } else if (element.toUpperCase() === element) {
+            console.log(`${element} is uppercase`);
+        } else {
+            console.log(`${element} is lowercase`);
+        }
+    });
+}
+caseChecker('bJI*ENsd4fRAIR5VIrsRFA-ijgRar7');
