6f9be5b2b9ebbd3a600c53426c96be21c4a6edcb
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-01-20 16:21:18 -0600

    added assets, finished jquery exercises, finished konami exercise - adding bonus styling

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -9,13 +9,19 @@ $(document).ready(function() {
     // Use jQuery to select an element by the id. Alert the contents of the element.
     let descriptionContents = $('#description').html();
     alert(descriptionContents)
+
     // CLASS SELECTORS
     // Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
     $('.codeup').css('border', 'solid 1px red');
+
     //ELEMENT SELECTORS
     // Using jQuery, set the font-size of all li elements to 20px.
     // $('li').css('font-size', '20px');
     // Craft selectors that highlight all the h1, p, and li elements.
+    // $('h1').css('background-color', '#fffd7a');
+    // $('p').css('background-color', '#fffd7a');
+    // $('li').css('background-color', '#fffd7a');
+
     // MULTIPLE SELECTORS
     // Combine your selectors that highlight all the h1, p, and li elements.
     $('h1, p, li').css('background-color', '#fffd7a');
