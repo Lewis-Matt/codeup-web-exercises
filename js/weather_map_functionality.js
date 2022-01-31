d49234c58fb3ff85ee928b4b8dbbe9d5a630169b
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-01-31 17:00:37 -0600

    finished overall layout for mobile and desktop, added map to page

--- js/weather_map_functionality.js
+++ js/weather_map_functionality.js
@@ -1,1 +1,8 @@
 "use strict";
+mapboxgl.accessToken = MAPBOX_KEY;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-streets-v11',
+    zoom: 1,
+    center: [-103.771556, 44.967243]
+});
