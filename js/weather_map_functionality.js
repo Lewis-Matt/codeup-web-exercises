6da615005338c311eb362bb3a1cb4d2a8fe73109
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-01 17:08:26 -0600

    functionality is complete

--- js/weather_map_functionality.js
+++ js/weather_map_functionality.js
@@ -1,48 +1,58 @@
 "use strict";
 (function () {
+    // Initialize weather/location data to a default location (San Antonio)
+    let lon = -98.489765;
+    let lat = 29.426742;
+    let defaultCords = {
+        lng: lon,
+        lat: lat
+    }
+
     // ADDS MAP TO #MAP DIV
     mapboxgl.accessToken = MAPBOX_KEY;
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/satellite-streets-v11',
-        zoom: 1,
-        center: [-103.771556, 44.967243]
+        zoom: 14,
+        center: [lon, lat],
     });
 
     // MAPBOX GEOCODER CONTROL API: ADDS SEARCH BOX TO MAP
     // NOTE: Input field class: .mapboxgl-ctrl-geocoder--input
     const geocoder = new MapboxGeocoder({
-        accessToken: MAPBOX_KEY,
-        // Marker placed and styling
+        accessToken: MAPBOX_KEY, // Marker placed and styling
         marker: {
             color: 'orange',
-        },
-        mapboxgl: mapboxgl
+        }, mapboxgl: mapboxgl
     }).setLimit(1);
     // NOTE: I set a limit to the number of results displayed, as the array created was random, and I could not reliably access the correct index to pass the lat/lon to the weather call
 
-    // Adds a marker wherever the user clicks
+    // GEOCODER SEARCH BOX ADDED TO NAV ELEMENT
+    document.getElementById('geocoder-search').appendChild(geocoder.onAdd(map));
+
+    // ADDS A MARKER WHEREVER THE USER CLICKS AND UPDATES WEATHER
     let marker = new mapboxgl.Marker();
 
     function add_marker(event) {
         let coordinates = event.lngLat;
+        console.log(coordinates)
         console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
         marker.setLngLat(coordinates).addTo(map);
+        lat = coordinates.lat;
+        lon = coordinates.lng;
+        endpoint = `${OPEN_WEATHER.base}onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely&appid=${OPEN_WEATHER.key}`;
+        // Reverse geocode to display clicked marker as the new location
+        reverseGeocode(coordinates, MAPBOX_KEY).then(function (results) {
+            $('#current-city').text(results);
+        })
+        getWeather(endpoint)
     }
 
     map.on('click', add_marker);
-
-    // Geocoder search box added to nav element
-    document.getElementById('geocoder-search').appendChild(geocoder.onAdd(map));
-
-    // Init geoJSON data to a default location (San Antonio)
-    let lon = -98.4951;
-    let lat = 29.4246;
-
-    // FETCHES WEATHER OBJECT FOR GIVEN LAT/LON DECIMAL COORDINATES
+    // FETCHES WEATHER OBJECT FOR GIVEN DEFAULT COORDINATES
     let endpoint = `${OPEN_WEATHER.base}onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely&appid=${OPEN_WEATHER.key}`;
 
-    // GETS WEATHER
+    // GETS WEATHER FOR COORDINATES
     function getWeather(url) {
         fetch(url)
             .then(response => {
@@ -51,6 +61,11 @@
             .then(displayWeather);
     }
 
+    // GET NAME OF DEFAULT LOCATION
+    reverseGeocode(defaultCords, MAPBOX_KEY).then(function (results) {
+        $('#current-city').text(results);
+    });
+    // GET WEATHER FOR DEFAULT LOCATION
     getWeather(endpoint)
 
     // GETS THE GEOJSON OBJECT FOR THE GEOCODER SEARCH QUERY
@@ -70,10 +85,22 @@
 
         // ADD CITY TO HTML
         $('#current-city').text(results.features[0].place_name);
-
-
     });
 
+    function reverseGeocode(coordinates, token) {
+        let baseUrl = 'https://api.mapbox.com';
+        let endPoint = '/geocoding/v5/mapbox.places/';
+        return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+            .then(function (res) {
+                return res.json();
+            })
+            // to get all the data from the request, comment out the following three lines...
+            .then(function (data) {
+                return data.features[0].place_name;
+            });
+    }
+
+    // ADDS WEATHER TO HTML ELEMENTS
     function displayWeather(response) {
         // TODAY
         $('#current-temp').text(response.current.temp)
