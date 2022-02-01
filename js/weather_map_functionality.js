f112d433b9c337ac54a4ad12af0f1982dfc0f9bb
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-01 10:06:05 -0600

    successfully implemented geocoder search bar, and returned the geoJSON data to be used for the weather lat/lon

--- js/weather_map_functionality.js
+++ js/weather_map_functionality.js
@@ -8,11 +8,28 @@ const map = new mapboxgl.Map({
     center: [-103.771556, 44.967243]
 });
 
-// FETCHES WEATHER OBJECT FOR GIVEN LAT/LON DECIMAL COORDINATES
+// FETCHES WEATHER OBJECT FOR GIVEN LAT/LON DECIMAL COORDINATES (auto parses JSON)
 fetch(
     `${OPEN_WEATHER.base}weather?lat=44.967243&lon=-103.771556&units=imperial&appid=${OPEN_WEATHER.key}`,
-    { method: 'GET' }
+    {method: 'GET'}
 )
-    .then( response => response.json() )
-    .then( json => console.log(json) )
-    .catch( error => console.error('error:', error) );
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.error('There was an error:', error));
+
+
+// MAPBOX GEOCODER CONTROL API - ADDS SEARCH BOX TO MAP
+// Input field class: .mapboxgl-ctrl-geocoder--input
+const geocoder = new MapboxGeocoder({
+    accessToken: MAPBOX_KEY,
+    marker: {
+        color: 'orange',
+    },
+    mapboxgl: mapboxgl
+});
+map.addControl(geocoder);
+
+// GETS THE GEOJSON OBJECT FOR THE GEOCODER SEARCH QUERY
+geocoder.on('results', function(results) {
+    console.log(results);
+})
