5dfd7c596b254383609890d7b9f6651ffec41f84
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-01 16:05:10 -0600

    added date-builder.js, called date-builder, html gets updated with weather from geocoder

---
+++ js/date-builder.js
@@ -0,0 +1,56 @@
+"use strict";
+
+//https://stackoverflow.com/questions/10032456/how-to-get-next-seven-days-from-x-and-format-in-js
+function GetDates(startDate, daysToAdd) {
+    let aryDates = [];
+    // i = 0 includes today
+    for (let i = 1; i <= daysToAdd; i++) {
+        let currentDate = new Date();
+        currentDate.setDate(startDate.getDate() + i);
+        // Array of days only
+        aryDates.push(DayAsString(currentDate.getDay()));
+        // Original
+        /*        aryDates.push(DayAsString(currentDate.getDay()) + ", " + currentDate.getDate() + " " + MonthAsString(currentDate.getMonth()) + " " + currentDate.getFullYear());*/
+    }
+
+    return aryDates;
+}
+
+function MonthAsString(monthIndex) {
+    let d = new Date();
+    let month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    return month[monthIndex];
+}
+
+function DayAsString(dayIndex) {
+    let weekdays = new Array(7);
+    weekdays[0] = "Sunday";
+    weekdays[1] = "Monday";
+    weekdays[2] = "Tuesday";
+    weekdays[3] = "Wednesday";
+    weekdays[4] = "Thursday";
+    weekdays[5] = "Friday";
+    weekdays[6] = "Saturday";
+
+    return weekdays[dayIndex];
+}
+
+// Today
+let startDate = new Date();
+// Next 5 days
+let aryDates = GetDates(startDate, 5);
+console.log(aryDates);
+
--- js/weather_map_functionality.js
+++ js/weather_map_functionality.js
@@ -1,48 +1,121 @@
 "use strict";
-// ADDS MAP TO #MAP DIV
-mapboxgl.accessToken = MAPBOX_KEY;
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-streets-v11',
-    zoom: 1,
-    center: [-103.771556, 44.967243]
-});
+(function () {
+    // ADDS MAP TO #MAP DIV
+    mapboxgl.accessToken = MAPBOX_KEY;
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/satellite-streets-v11',
+        zoom: 1,
+        center: [-103.771556, 44.967243]
+    });
 
-// FETCHES WEATHER OBJECT FOR GIVEN LAT/LON DECIMAL COORDINATES
-// Openweather 'onecall' api call
-const endpoint = `${OPEN_WEATHER.base}onecall?lat=33.4&lon=-94.04&units=imperial&exclude=minutely&appid=${OPEN_WEATHER.key}`;
-fetch(endpoint,
-    {method: 'get'}
-)
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error => console.error('There was an error:', error));
+    // MAPBOX GEOCODER CONTROL API: ADDS SEARCH BOX TO MAP
+    // NOTE: Input field class: .mapboxgl-ctrl-geocoder--input
+    const geocoder = new MapboxGeocoder({
+        accessToken: MAPBOX_KEY,
+        // Marker placed and styling
+        marker: {
+            color: 'orange',
+        },
+        mapboxgl: mapboxgl
+    }).setLimit(1);
+    // NOTE: I set a limit to the number of results displayed, as the array created was random, and I could not reliably access the correct index to pass the lat/lon to the weather call
 
-// MAPBOX GEOCODER CONTROL API: ADDS SEARCH BOX TO MAP
-// NOTE: Input field class: .mapboxgl-ctrl-geocoder--input
-const geocoder = new MapboxGeocoder({
-    accessToken: MAPBOX_KEY,
-    // Marker placed and styling
-    marker: {
-        color: 'orange',
-    },
-    mapboxgl: mapboxgl
-}).setLimit(1);
-// NOTE: I set a limit to the number of results displayed, as the array created was random, and I could not reliably access the correct index to pass the lat/lon to the weather call
+    // Adds a marker wherever the user clicks
+    let marker = new mapboxgl.Marker();
 
-// Geocoder search box in nav element
-document.getElementById('geocoder-search').appendChild(geocoder.onAdd(map));
+    function add_marker (event) {
+        let coordinates = event.lngLat;
+        console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
+        marker.setLngLat(coordinates).addTo(map);
+    }
+
+    map.on('click', add_marker);
 
 
-// References the geoJSON lat/lon returned from the Mapbox geocoder
-let coordsFromGeocoder = [];
 
-// GETS THE GEOJSON OBJECT FOR THE GEOCODER SEARCH QUERY
-geocoder.on('results', function (results) {
-    console.log(results);
-    // Gets the coordinates from geoJSON object
-    // Note: need to reverse the coordinates as mapbox gl uses lon/lat, openweather uses lat/lon
-    // The geocoder results.features[] is an array of the top 5 matches/suggestions.
-    // results.features.forEach(){};
-});
+
+    // Geocoder search box added to nav element
+    document.getElementById('geocoder-search').appendChild(geocoder.onAdd(map));
+
+
+
+
+
+    // Declares the geoJSON lat/lon variable returned from the Mapbox geocoder
+    let lon;
+    let lat;
+
+
+    // GETS THE GEOJSON OBJECT FOR THE GEOCODER SEARCH QUERY
+    geocoder.on('results', function (results) {
+        console.log(results);
+        // Gets the coordinates from geoJSON object
+        // Note: mapbox gl/geoJSON uses lon/lat, openweather uses lat/lon
+        // The geocoder results.features[] is an array of the top 5 matches/suggestions.
+        lon = results.features[0].geometry.coordinates[0];
+        lat = results.features[0].geometry.coordinates[1];
+        console.log(`Search result longitude: ${lon}`);
+        console.log(`Search result latitude: ${lat}`);
+
+        // FETCHES WEATHER OBJECT FOR GIVEN LAT/LON DECIMAL COORDINATES
+        let endpoint = `${OPEN_WEATHER.base}onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely&appid=${OPEN_WEATHER.key}`;
+
+        function getWeather(url) {
+            fetch(url)
+                .then(response => {
+                    return response.json()
+                })
+                .then(displayWeather);
+        }
+
+        getWeather(endpoint);
+
+        // ADD CITY TO HTML
+        $('#current-city').text(results.features[0].place_name);
+
+        function displayWeather(response) {
+            // TODAY
+            $('#current-temp').text(response.current.temp)
+            $('#current-conditions').text(response.current.weather[0].description)
+            $('#currentHI').text(response.daily[0].temp.max)
+            $('#currentLO').text(response.daily[0].temp.min)
+
+            // FIVE-DAY FORECAST
+            // Day 1
+            let day1 = response.daily[1];
+            $('#weekday1').text(aryDates[0])
+            $('#icon1').text(day1.weather[0].description)
+            $('#low1').text(day1.temp.min)
+            $('#hi1').text(day1.temp.max)
+            // Day 2
+            let day2 = response.daily[2];
+            $('#weekday2').text(aryDates[1])
+            $('#icon2').text(day1.weather[0].description)
+            $('#low2').text(day1.temp.min)
+            $('#hi2').text(day1.temp.max)
+            // Day 3
+            let day3 = response.daily[3];
+            $('#weekday3').text(aryDates[2])
+            $('#icon3').text(day3.weather[0].description)
+            $('#low3').text(day3.temp.min)
+            $('#hi3').text(day3.temp.max)
+            // Day 4
+            let day4 = response.daily[4];
+            $('#weekday4').text(aryDates[3])
+            $('#icon4').text(day4.weather[0].description)
+            $('#low4').text(day4.temp.min)
+            $('#hi4').text(day4.temp.max)
+            // Day 5
+            let day5 = response.daily[5];
+            $('#weekday5').text(aryDates[4])
+            $('#icon5').text(day5.weather[0].description)
+            $('#low5').text(day5.temp.min)
+            $('#hi5').text(day5.temp.max)
+        }
+
+    });
+
+})();
+
 
