07e778d6b5052c52ad739a663dd75181a587db49
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-01 09:44:51 -0600

    made weather api and object with key and base url, test call to weather api to get weather obj for given lat/lon coordinates

--- js/weather_map_functionality.js
+++ js/weather_map_functionality.js
@@ -1,4 +1,5 @@
 "use strict";
+// ADDS MAP TO #MAP DIV
 mapboxgl.accessToken = MAPBOX_KEY;
 const map = new mapboxgl.Map({
     container: 'map',
@@ -6,3 +7,12 @@ const map = new mapboxgl.Map({
     zoom: 1,
     center: [-103.771556, 44.967243]
 });
+
+// FETCHES WEATHER OBJECT FOR GIVEN LAT/LON DECIMAL COORDINATES
+fetch(
+    `${OPEN_WEATHER.base}weather?lat=44.967243&lon=-103.771556&units=imperial&appid=${OPEN_WEATHER.key}`,
+    { method: 'GET' }
+)
+    .then( response => response.json() )
+    .then( json => console.log(json) )
+    .catch( error => console.error('error:', error) );
