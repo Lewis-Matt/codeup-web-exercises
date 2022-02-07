54492365956216fba746f0b6ae721ab5a938be50
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-07 11:19:23 -0600

    pseudo, link to API Token

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// TODO: Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made.
+
+// The Events API is a read-only API to the GitHub events.
+// To get the list of random users, you have to make a request to the /user endpoint, and to get the details of a specific user, you make a request to the /user/{username} endpoint.
+
+
+// PSEUDO:
+// Let the user enter a Github username
+// Store the username in a variable
+// Fetch the JSON
+fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
+
+
+
+// TODO: Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
