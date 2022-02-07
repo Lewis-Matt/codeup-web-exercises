3f5c1fbefbf0ca42b6a84afdb45a009714c7fe4b
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-07 14:29:01 -0600

    typo

--- js/promises.js
+++ js/promises.js
@@ -43,7 +43,6 @@ function commitDate() {
     document.getElementById("commit").innerHTML = userLastCommit(githubName);
 }
 
-
 // TODO: Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
 
 function wait(num) {
